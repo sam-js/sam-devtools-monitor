c39e449493186f748966d0fd67e2ccb2c0e4e6fa
Gunar C. Gessner <gunar@gunargessner.com>
2016-03-04 16:49:47 -0300

    Remove useless html and make optimizations

--- src/index.js
+++ src/index.js
@@ -15,37 +15,39 @@ const style = {
     cursor: pointer`,
 }
 
-const render = (snapshots, loadSnapshot) => {
+const keys = ['@@nap']
+
+const filterKeys = store => {
+  const output = { ...store }
+  keys.forEach(k => delete output[k])
+  return output
+}
+
+const getNode = _ => {
   let node = document.getElementById('SAMTimeTravelUI')
   if (!node) {
     node = document.createElement('div')
     node.style = style.dock
     document.body.appendChild(node)
   }
-  
+  return node
+}
+
+const render = (snapshots, loadSnapshot) => {
+  const node = getNode()
   snapshots.forEach((snapshot, i) => {
-    const store = snapshot.store
-    let dataset = { ...snapshot.dataset }
+    const { store, dataset } = snapshot
     const el = document.createElement('div')
-    el.addEventListener('click', e => {
-      console.log('click', i, e)
-      loadSnapshot(i)
-    })
-    let html = `<div id="sam-devtools-snapshot${i}" style="${style.snapshot}">`
-    if (dataset['@@nap']) {
-      delete dataset['@@nap']
-      html = html + `<strong>From NAP</strong>`
-    }
-    html = html + `
-        <pre>present(${JSON.stringify(dataset)})</pre>
-        <pre>Store -> ${JSON.stringify(store)}</pre>`
+    el.addEventListener('click', loadSnapshot.bind(null, i))
+    el.style = style.snapshot
+    let html = (dataset['@@nap'] ? `<strong>NAP</strong>` : '')
+    const filteredStore = filterKeys(store)
+    html = html + `<pre>present(${JSON.stringify(dataset)})</pre><pre>Store -> ${JSON.stringify(store)}</pre>`
     el.innerHTML = html
     node.appendChild(el)
   })
-
   // Scroll to bottom
   node.scrollTop = node.scrollHeight
-
 }
 
 export default render
